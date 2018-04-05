48b340f1c9a88898e39bb57a52ff32794972f663
Steven Starwalt <steven.starwalt@yahoo.com>
2018-04-05 16:13:10 -0700

    added the first 2 lab items

---
+++ sample_data/__init__.py
@@ -0,0 +1,123 @@
+MOCK DATA = [
+    {
+        "symbol": "NM",
+        "companyName": "Not Me",
+        "exchange": "New York Stock Exchange",
+        "industry": "Social media",
+        "website": "http://www.notme.com",
+        "description": "Not me is a company that will provide online excuses for you to use in any situation.",
+        "CEO": "Alfred E. Newman",
+        "issueType": "cs",
+        "sector": "Tech"
+    },
+    {
+        "symbol": "GE",
+        "companyName": "General Electric Company",
+        "exchange": "New York Stock Exchange",
+        "industry": "Industrial Products",
+        "website": "http://www.ge.com",
+        "description": "General Electric Co is a digital industrial company. It operates in various segments, including power and water, oil and gas, energy management, aviation, healthcare, transportation, appliances and lighting, and more.",
+        "CEO": "John L. Flannery",
+        "issueType": "cs",
+        "sector": "Industrials"
+    },
+    {
+        "symbol": "GE",
+        "companyName": "General Electric Company",
+        "exchange": "New York Stock Exchange",
+        "industry": "Industrial Products",
+        "website": "http://www.ge.com",
+        "description": "General Electric Co is a digital industrial company. It operates in various segments, including power and water, oil and gas, energy management, aviation, healthcare, transportation, appliances and lighting, and more.",
+        "CEO": "John L. Flannery",
+        "issueType": "cs",
+        "sector": "Industrials"
+    },
+    {
+        "symbol": "GE",
+        "companyName": "General Electric Company",
+        "exchange": "New York Stock Exchange",
+        "industry": "Industrial Products",
+        "website": "http://www.ge.com",
+        "description": "General Electric Co is a digital industrial company. It operates in various segments, including power and water, oil and gas, energy management, aviation, healthcare, transportation, appliances and lighting, and more.",
+        "CEO": "John L. Flannery",
+        "issueType": "cs",
+        "sector": "Industrials"
+    },
+    {
+        "symbol": "GE",
+        "companyName": "General Electric Company",
+        "exchange": "New York Stock Exchange",
+        "industry": "Industrial Products",
+        "website": "http://www.ge.com",
+        "description": "General Electric Co is a digital industrial company. It operates in various segments, including power and water, oil and gas, energy management, aviation, healthcare, transportation, appliances and lighting, and more.",
+        "CEO": "John L. Flannery",
+        "issueType": "cs",
+        "sector": "Industrials"
+    },
+    {
+        "symbol": "GE",
+        "companyName": "General Electric Company",
+        "exchange": "New York Stock Exchange",
+        "industry": "Industrial Products",
+        "website": "http://www.ge.com",
+        "description": "General Electric Co is a digital industrial company. It operates in various segments, including power and water, oil and gas, energy management, aviation, healthcare, transportation, appliances and lighting, and more.",
+        "CEO": "John L. Flannery",
+        "issueType": "cs",
+        "sector": "Industrials"
+    },
+    {
+        "symbol": "GE",
+        "companyName": "General Electric Company",
+        "exchange": "New York Stock Exchange",
+        "industry": "Industrial Products",
+        "website": "http://www.ge.com",
+        "description": "General Electric Co is a digital industrial company. It operates in various segments, including power and water, oil and gas, energy management, aviation, healthcare, transportation, appliances and lighting, and more.",
+        "CEO": "John L. Flannery",
+        "issueType": "cs",
+        "sector": "Industrials"
+    },
+    {
+        "symbol": "GE",
+        "companyName": "General Electric Company",
+        "exchange": "New York Stock Exchange",
+        "industry": "Industrial Products",
+        "website": "http://www.ge.com",
+        "description": "General Electric Co is a digital industrial company. It operates in various segments, including power and water, oil and gas, energy management, aviation, healthcare, transportation, appliances and lighting, and more.",
+        "CEO": "John L. Flannery",
+        "issueType": "cs",
+        "sector": "Industrials"
+    },
+   {
+        "symbol": "GE",
+        "companyName": "General Electric Company",
+        "exchange": "New York Stock Exchange",
+        "industry": "Industrial Products",
+        "website": "http://www.ge.com",
+        "description": "General Electric Co is a digital industrial company. It operates in various segments, including power and water, oil and gas, energy management, aviation, healthcare, transportation, appliances and lighting, and more.",
+        "CEO": "John L. Flannery",
+        "issueType": "cs",
+        "sector": "Industrials"
+    },
+   {
+        "symbol": "GE",
+        "companyName": "General Electric Company",
+        "exchange": "New York Stock Exchange",
+        "industry": "Industrial Products",
+        "website": "http://www.ge.com",
+        "description": "General Electric Co is a digital industrial company. It operates in various segments, including power and water, oil and gas, energy management, aviation, healthcare, transportation, appliances and lighting, and more.",
+        "CEO": "John L. Flannery",
+        "issueType": "cs",
+        "sector": "Industrials"
+    },
+   {
+        "symbol": "GE",
+        "companyName": "General Electric Company",
+        "exchange": "New York Stock Exchange",
+        "industry": "Industrial Products",
+        "website": "http://www.ge.com",
+        "description": "General Electric Co is a digital industrial company. It operates in various segments, including power and water, oil and gas, energy management, aviation, healthcare, transportation, appliances and lighting, and more.",
+        "CEO": "John L. Flannery",
+        "issueType": "cs",
+        "sector": "Industrials"
+    },
+]
