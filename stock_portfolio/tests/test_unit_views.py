4d746ce88618c0e71c7b2bd4a760e732234fa84d
Steven Starwalt <steven.starwalt@yahoo.com>
2018-04-07 16:22:30 -0700

    tryed to get the tests working

--- stock_portfolio/tests/test_unit_views.py
+++ stock_portfolio/tests/test_unit_views.py
@@ -1,9 +1,52 @@
-def test_default_behavior_of_base_route(dummy_request):
-    from ..views.default import home_view
-    from pyramid.response import Response
+def test_default_behavior_of_home_view(dummy_request):
+    from ..views.default import my_home_view
 
     request = dummy_request
-    response = home_view(request)
-    assert isinstance(response, Response)
+    response = my_home_view(request)
+    assert isinstance(response, dict)
+    assert response == {}
 
-# def test_sign
+
+def test_default_behavior_of_auth_view(dummy_request):
+    from ..views.default import my_auth_view
+
+    response = my_auth_view(dummy_request)
+    assert isinstance(response, dict)
+    assert response == {}
+
+
+def test_post_auth_view(dummy_request):
+    from ..views.default import my_auth_view
+    from pyramid.httpexceptions import HTTPNotFound
+
+    request = dummy_request
+    request.method = 'POST'
+    request.POST = {'username': 'dummy', 'email': 'wat@wat.com', 'password': 'pass'}
+
+    response = my_auth_view(request)
+
+    assert isinstance(response, HTTPNotFound)
+
+
+def test_default_behavior_of_portfolio_view(dummy_request):
+    from ..views.default import my_portfolio_view
+
+    response = my_portfolio_view(dummy_request)
+    assert type(response) == dict
+    assert response['entries'][0]['symbol'] == 'GE'
+
+
+# def test_default_behavior_of_detail_view(dummy_request):
+#     from ..views.default import my_detail_view
+
+#     response = my_detail_view(dummy_request)
+#     assert isinstance(response, dict)
+#     assert response == {}
+
+
+# def test_default_behavior_of_stock_view(dummy_request):
+#     from ..views.default import my_stock_view
+
+#     response = my_stock_view(dummy_request)
+#     assert isinstance(response, dict)
+#     assert response == {}
