8958869d851e8f006eabc7c647ec062d9cb6b37c
Steven Starwalt <steven.starwalt@yahoo.com>
2018-04-10 10:19:07 -0700

    added some testing code, tried to make css changes

--- stock_portfolio/tests/conftest.py
+++ stock_portfolio/tests/conftest.py
@@ -5,3 +5,14 @@ from pyramid import testing
 @pytest.fixture
 def dummy_request():
     return testing.DummyRequest()
+
+
+# @pytest.fixture
+# def confiuration(request):
+
+#     config = testing.setUp(settings={
+#         # 'sqlalchemy.url': 'postgres://localhost:5432/entries_test'
+          
+#     })
+    #   config.include('stock_portfolio.models')
+    #   config.include('stock_portfolio.routes')
