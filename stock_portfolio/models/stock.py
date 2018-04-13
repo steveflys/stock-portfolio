95bd3e1fb1a56b91f7ddd006af7c538d6f0c14e6
Steven Starwalt <steven.starwalt@yahoo.com>
2018-04-12 18:45:16 -0700

    removed the foregin key from Stocks

--- stock_portfolio/models/stock.py
+++ stock_portfolio/models/stock.py
@@ -8,10 +8,10 @@ from .meta import Base
 from .association import association_table
 
 
+
 class Stock(Base):
     __tablename__ = 'stock'
     id = Column(Integer, primary_key=True)
-    account_id = Column(Text, ForeignKey('accounts.username', nullable=False))
     symbol = Column(String, nullable=False, unique=True)
     companyName = Column(String)
     CEO = Column(String)
